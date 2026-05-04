32a8360fea1f010ff6cd35b393e9539afe49d0bc
QK user2 <QK user2@AUTOMATSCRIPTS.SLIC.COM>
2026-05-04 18:37:38 +0530

    v1

--- Scripts/Claim_Registration/Script1773555270780.groovy
+++ Scripts/Claim_Registration/Script1773555270780.groovy
@@ -62,8 +62,9 @@ WebUI.waitForElementVisible(findTestObject('Claim Intimation Shrilife/Page_Death
 
 WebUI.click(findTestObject('Claim Intimation Shrilife/Page_Death Claims - SHRILIFE/Open Claims'))
 
-WebUI.delay(2)
+WebUI.delay(3)
 
+WebUI.waitForElementVisible(findTestObject('Claim Intimation Shrilife/Page_Death Claims - SHRILIFE/grid_table'), 15)
 
 WebUI.setText(findTestObject('Claim Intimation Shrilife/Page_Death Claims - SHRILIFE/input_policy_no'), data['PolicyNumber'])
 
